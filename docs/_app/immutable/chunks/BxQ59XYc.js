1b6c6f07daaa71e3c168aaaa9ee5e24ade75e565
profmancusoa <profmancusoa@gmail.com>
2025-01-20 11:23:45 +0100

    primo push

---
+++ docs/_app/immutable/chunks/BxQ59XYc.js
@@ -0,0 +1,1 @@
+import{e}from"./5rj1CUhs.js";e();
---
+++ docs/_app/immutable/nodes/3.CMUxW0eQ.js
@@ -0,0 +1,1 @@
+import{a as t,t as p}from"../chunks/HZ8MweiI.js";import"../chunks/BxQ59XYc.js";var r=p("<h1>ABOUT PAGE</h1>");function n(a){var o=r();t(a,o)}export{n as component};
